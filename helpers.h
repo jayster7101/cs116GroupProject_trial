bcb69f22451c9e27573afd9991bb32711f0ee282
jayden jardine <jayster7101@gmail.com>
2022-05-07 14:53:56 -0700

    adding algo function to path_find_g but doesnt work correctly

--- helpers.h
+++ helpers.h
@@ -9,10 +9,11 @@ int path();
 void read_line(std::string, Node**); // opens file, getsline, passes line to parse_line
 void parse_line(std::string, int, int, Node**); // receives line and assigns data to new node, assigns node to spot in matrix
 //template<class T>
-void path_find_s(double &, int, Node**, Position<int> current);
-void path_find_g(double &, int, Node**, Position<double> current);
+void path_find_s(double &, int, Node**, Position<int> current, int);
+void path_find_g(double &, int, Node**, Position<double> current, int);
 void print_map(Node**);
 //void populate_matrix(**);
+int get_size(std::string);
 void get_trucks(double &gas, int &capacity);
 bool whichtruck();
 void display_truck();
--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@
  int main()
  {  
     double gas;
-    int SIZE = 5;
     int capacity;
-    Node** matrix = new Node*[SIZE];
     std::string _file = get_file();
+    int SIZE = get_size(_file);
+    Node** matrix = new Node*[SIZE];
     read_line(_file, matrix);
     get_trucks(gas,capacity);
     Position<double> start(0);
-    path_find_g(gas,capacity,matrix,start);
+    path_find_g(gas,capacity,matrix,start,SIZE);
     print_map( matrix);
  }

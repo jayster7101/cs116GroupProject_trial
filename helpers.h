0bca352905b50fdff1a1389ae36f3afbc4a252da
Jayden <82332696+jayster7101@users.noreply.github.com>
2022-05-07 01:22:12 +0000

    updated algo, added users data into algo

--- helpers.h
+++ helpers.h
@@ -10,8 +10,14 @@ void read_line(std::string, Node**); // opens file, getsline, passes line to par
 void parse_line(std::string, int, int, Node**); // receives line and assigns data to new node, assigns node to spot in matrix
 //template<class T>
 void path_find_s(double &, int, Node**, Position<int> current);
+void path_find_g(double &, int, Node**, Position<double> current);
 void print_map(Node**);
 //void populate_matrix(**);
+void get_trucks(double &gas, int &capacity);
+bool whichtruck();
+void display_truck();
+bool our_truck(double &gas, int &capacity);
+void own_truck(double &gas, int &capacity);
  
 
 
